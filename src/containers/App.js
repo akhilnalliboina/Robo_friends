aa0767834383dadfd24794aef42cb807fb6dbbea
akhiln95 <akhilnalliboina@gmail.com>
2021-02-17 06:06:10 +0100

    Title change

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -34,9 +33,7 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
             <CardList robots={filteredRobots} />
-          </Scroll>
         </div>
       );
   }
